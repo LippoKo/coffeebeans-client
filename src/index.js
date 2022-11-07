89019e7c1af6035ba9746f56f9d7e92328d179cd
LippoKo <luizlimadevpro@gmail.com>
2022-11-07 20:09:36 +0100

    add mobile popup

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
-import { AuthProviderWrapper } from "./context/auth.context";
-import { NextUIProvider } from "@nextui-org/react";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter as Router } from "react-router-dom"
+import App from "./App"
+import { AuthProviderWrapper } from "./context/auth.context"
+import "./index.css"
+import reportWebVitals from "./reportWebVitals"
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-	<React.StrictMode>
-		<Router>
-			<AuthProviderWrapper>
-				<App />
-			</AuthProviderWrapper>
-		</Router>
-	</React.StrictMode>
-);
+  <React.StrictMode>
+    <Router>
+      <AuthProviderWrapper>
+        <App />
+      </AuthProviderWrapper>
+    </Router>
+  </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,103 +1,103 @@
-import { useState } from "react";
-import axios from "axios";
-import { useNavigate, Link } from "react-router-dom";
+import axios from "axios"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 export default function SignupPage() {
-	const [firstName, setFirstName] = useState("");
-	const [lastName, setLastName] = useState("");
-	const [email, setEmail] = useState("");
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-	const [errorMessage, setErrorMessage] = useState(null);
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [errorMessage, setErrorMessage] = useState(null)
 
-	const navigate = useNavigate();
+  const navigate = useNavigate()
 
-	const handleFirstName = (e) => setFirstName(e.target.value);
-	const handleLastName = (e) => setLastName(e.target.value);
-	const handleEmail = (e) => setEmail(e.target.value);
-	const handleUsername = (e) => setUsername(e.target.value);
-	const handlePassword = (e) => setPassword(e.target.value);
+  const handleFirstName = (e) => setFirstName(e.target.value)
+  const handleLastName = (e) => setLastName(e.target.value)
+  const handleEmail = (e) => setEmail(e.target.value)
+  const handleUsername = (e) => setUsername(e.target.value)
+  const handlePassword = (e) => setPassword(e.target.value)
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
-		const newUser = { firstName, lastName, email, username, password };
+    const newUser = { firstName, lastName, email, username, password }
 
-		axios
-			.post(`${process.env.REACT_APP_API_URL}/auth/signup`, newUser)
-			.then((response) => {
-				navigate("/login");
-			})
-			.catch((err) => {
-				console.log(err);
-				setErrorMessage(err.response.data.errorMessage);
-			});
-	};
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/auth/signup`, newUser)
+      .then((response) => {
+        navigate("/login")
+      })
+      .catch((err) => {
+        console.log(err)
+        setErrorMessage(err.response.data.errorMessage)
+      })
+  }
 
-	return (
-		<div className="cont">
-			<h2 className="text-2xl font-bold">SignUp</h2>
-			<div className="SignupPage">
-				<form onSubmit={handleSubmit}>
-					<label htmlFor="firstname">
-						FirstName:
-						<input
-							type="text"
-							name="firstname"
-							value={firstName}
-							onChange={handleFirstName}
-						/>
-					</label>
+  return (
+    <div className="cont">
+      <h2 className="text-2xl font-bold">SignUp</h2>
+      <div className="SignupPage">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="firstname">
+            FirstName:
+            <input
+              type="text"
+              name="firstname"
+              value={firstName}
+              onChange={handleFirstName}
+            />
+          </label>
 
-					<label htmlFor="lastname">
-						LastName:
-						<input
-							type="text"
-							name="lastname"
-							value={lastName}
-							onChange={handleLastName}
-						/>
-					</label>
+          <label htmlFor="lastname">
+            LastName:
+            <input
+              type="text"
+              name="lastname"
+              value={lastName}
+              onChange={handleLastName}
+            />
+          </label>
 
-					<label htmlFor="email">
-						Email:
-						<input
-							type="email"
-							name="email"
-							value={email}
-							onChange={handleEmail}
-						/>
-					</label>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={handleEmail}
+            />
+          </label>
 
-					<label htmlFor="username">
-						Username:
-						<input
-							type="text"
-							name="username"
-							value={username}
-							onChange={handleUsername}
-						/>
-					</label>
+          <label htmlFor="username">
+            Username:
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={handleUsername}
+            />
+          </label>
 
-					<label htmlFor="Password">
-						Password:
-						<input
-							type="password"
-							name="password"
-							value={password}
-							onChange={handlePassword}
-						/>
-					</label>
+          <label htmlFor="Password">
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={handlePassword}
+            />
+          </label>
 
-					<button className="btn btn-primary btn-xs">Sign Up</button>
-				</form>
+          <button className="btn btn-primary btn-xs">Sign Up</button>
+        </form>
 
-				{errorMessage && <p className="error">{errorMessage}</p>}
-				<p>Already have an account?</p>
-				<button className="btn btn-primary btn-xs">
-					<Link to="/login">Login</Link>
-				</button>
-			</div>
-		</div>
-	);
+        {errorMessage && <p className="error">{errorMessage}</p>}
+        <p>Already have an account?</p>
+        <button className="btn btn-primary btn-xs">
+          <Link to="/login">Login</Link>
+        </button>
+      </div>
+    </div>
+  )
 }
